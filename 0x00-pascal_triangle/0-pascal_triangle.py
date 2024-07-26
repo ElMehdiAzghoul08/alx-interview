98375e4a0587520a03edcecdf6dba7e7f07b7e93
El Mehdi <azghoulmehdi899@gmail.com>
2024-07-26 05:47:03 +0100

    first commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]
+    for i in range(1, n):
+        current_row = triangle[-1]
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(current_row[j-1] + current_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
+    
+    return triangle
