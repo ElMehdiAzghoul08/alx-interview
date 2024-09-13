d0ca218431af518aad3bf6221afec22644c4f20f
El Mehdi <azghoulmehdi899@gmail.com>
2024-09-13 17:04:44 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = require('request');
+const https = require('https');
 
 const movieId = process.argv[2];
 
@@ -11,39 +11,30 @@ if (!movieId) {
 
 const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request(filmUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Invalid status code:', response.statusCode);
-    return;
-  }
-
-  const film = JSON.parse(body);
-  const characters = film.characters;
-
-  const getCharacterName = (url) => {
-    return new Promise((resolve, reject) => {
-      request(url, (error, response, body) => {
-        if (error) reject(error);
-        else if (response.statusCode !== 200) {
-          reject(new Error(`Invalid status code: ${response.statusCode}`));
-        } else {
-          const character = JSON.parse(body);
-          resolve(character.name);
-        }
+function httpsGet (url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
       });
+      response.on('end', () => {
+        resolve(JSON.parse(data));
+      });
+    }).on('error', (error) => {
+      reject(error);
     });
-  };
+  });
+}
 
-  Promise.all(characters.map(getCharacterName))
-    .then((names) => {
-      names.forEach((name) => console.log(name));
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-});
+httpsGet(filmUrl)
+  .then(film => {
+    return film.characters.reduce((promise, url) => {
+      return promise.then(() => httpsGet(url))
+        .then(character => console.log(character.name))
+        .catch(error => console.error(`Failed to fetch character: ${error.message}`));
+    }, Promise.resolve());
+  })
+  .catch(error => {
+    console.error('Error:', error.message);
+  });
